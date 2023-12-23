62852e0f62795691206d828e538b9b34a1428ab1
Mahendra chavda <chavdamahendra247@gmail.com>
2023-12-23 12:03:32 +0530

    initial commit

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'client', 'dist'),
+      serveStaticOptions: {
+        setHeaders: (res: any, path: string, stat: any) => {
+          res.set('Access-Control-Allow-Origin', '*');
+        },
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
